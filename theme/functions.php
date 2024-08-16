bd750c508f5496902fe3d08806d2a52c2c243450
dkeesey <460435+dkeesey@users.noreply.github.com>
2024-08-16 10:29:30 -0700

    finish implementing inter font

--- theme/functions.php
+++ theme/functions.php
@@ -161,9 +161,13 @@ function megan_tw_scripts()
 		true
 	);
 
-	if (is_singular() && comments_open() && get_option('thread_comments')) {
-		wp_enqueue_script('comment-reply');
-	}
+	// Enqueue Inter font
+	// wp_enqueue_style(
+	// 	'inter-font',
+	// 	'https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap',
+	// 	[],
+	// 	null
+	// );
 }
 add_action('wp_enqueue_scripts', 'megan_tw_scripts');
 
@@ -246,3 +250,71 @@ require get_template_directory() . '/inc/template-tags.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
+
+// Disable support for comments and trackbacks in post types
+function megan_tw_disable_comments_post_types_support()
+{
+	$post_types = get_post_types();
+	foreach ($post_types as $post_type) {
+		if (post_type_supports($post_type, 'comments')) {
+			remove_post_type_support($post_type, 'comments');
+			remove_post_type_support($post_type, 'trackbacks');
+		}
+	}
+}
+add_action('admin_init', 'megan_tw_disable_comments_post_types_support');
+
+// Close comments on the front-end
+function megan_tw_disable_comments_status()
+{
+	return false;
+}
+add_filter('comments_open', 'megan_tw_disable_comments_status', 20, 2);
+add_filter('pings_open', 'megan_tw_disable_comments_status', 20, 2);
+
+// Hide existing comments
+function megan_tw_disable_comments_hide_existing_comments($comments)
+{
+	$comments = [];
+	return $comments;
+}
+add_filter(
+	'comments_array',
+	'megan_tw_disable_comments_hide_existing_comments',
+	10,
+	2
+);
+
+// Remove comments page in menu
+function megan_tw_disable_comments_admin_menu()
+{
+	remove_menu_page('edit-comments.php');
+}
+add_action('admin_menu', 'megan_tw_disable_comments_admin_menu');
+
+// Redirect any user trying to access comments page
+function megan_tw_disable_comments_admin_menu_redirect()
+{
+	global $pagenow;
+	if ($pagenow === 'edit-comments.php') {
+		wp_redirect(admin_url());
+		exit();
+	}
+}
+add_action('admin_init', 'megan_tw_disable_comments_admin_menu_redirect');
+
+// Remove comments metabox from dashboard
+function megan_tw_disable_comments_dashboard()
+{
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+}
+add_action('admin_init', 'megan_tw_disable_comments_dashboard');
+
+// Remove comments links from admin bar
+function megan_tw_disable_comments_admin_bar()
+{
+	if (is_admin_bar_showing()) {
+		remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+	}
+}
+add_action('init', 'megan_tw_disable_comments_admin_bar');
